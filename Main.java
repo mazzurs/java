f93e6f23b28ae3491bb2286fb90ecf3de10d7419
mazzurs <mazzurs@ya.ru>
2017-07-03 17:05:50 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,43 @@
 package sample;
-import java.util.ArrayList;
-
-/* Припускаємо, що у нас є якісь персони (Person) та розробники (Developer). Розробники мають усі властивості
-* персони, тільки вони мають ще додаткову властивість, вони можуть казати яку задачу вони виконують (метод develop).
-* Щоб не переписувати код для розробників ми наслідуємо розробників від персон.
-*       Інкапсуляція полягає у тому, що
-* ми приховуємо дані персон роблячи їх приватними, а для зчитування та додавання даних робимо гетери та конструктор.
-*       Поліморфізм полягає у перевизначенні метода changeId(), який повертає для персони id, до якого додає двійку, а для розробника
- *трійку.
- *      Також я привів приклад агрегації на прикладі машин і моторів. У машин можуть бути різні мотори з різною потужністю. Щоб
- *  не вказувати кожен раз, коли створюємо машину потужність мотора ми робимо, скажем так, коробку із моторами і потім при
- *  додаванні машини ми просто указуємо номер мотора який нам потрібно.
+/*Маємо дві версії квадрокоптера (Mavic_1 та Mavic_2). Mavic_2  це удосконалена версія квадрокоптера Mavic_1, тому
+* вона має усі функції свого батька (збільшити, зменшити висоту, та вивести її на екран) та ще додаткові, свої, функції
+* (збільшити, або зменшити швидкість польоту, зависнути у повітрі, та вивести швидкість польоту на екран). Поліморфізм полягає
+* у тому, що ми можемо використовувати пульт управління від старшої моделі квадрокоптера до новішого квадрокоптера, але не навпаки,
+*  так як ми не знаємо
+ * які функції можуть бути у новішого пульта.
+ *      Інкапсуляцію я виразив у способі реалізації внутрішнього механізму зміни висоти та швидкості польоту. Тільки
+ *      сам обєкт може змінити свій стан, попередньо перевіряючи умови обмеження, щоб не допустити перебування у
+ *      некоректному стані.
+*
+*
 * */
 
-public class Main {
+
+public class Main  {
 
 
     public static void main(String[] args)
-    {
-        Person p1 = new Person("Sergey", "Mazur", 1);
-        Person p2 = new Person("Vitaliy", "Jaroviy", 2);
+    { // Використовуємо пульт від квадрокоптера Mavic_2 до квадрокоптера Mavic_2
+        Mavic_2 mav = new Mavic_2();
+        mav.on();
+        mav.off();
+        mav.increaseSpeed();
+        mav.increaseSpeed();
+        mav.increaseSpeed();
+        System.out.println("Швидкість:" + mav.getSpeed());
+        mav.increaseHeight();
+        mav.increaseHeight();
+        System.out.println("Висота:" + mav.getHeight());
 
-        Developer d1 = new Developer ("Ruslan", "Bilmakovsky", 3);
-        Developer d2 = new Developer ("Ivan", "Kolisnik", 4);
-
-      System.out.println(p1.getFirstName() + " " + p1.getLastName()+" "+ p1.changeId());    // changeId добавляет 2 к id
-        System.out.println(d1.getFirstName() + " " + d1.getLastName()+ " "+ d1.changeId()); // changeId добавляет 3 к id
-
-        ArrayList<Engine> engines = new ArrayList();
-        engines.add(new Engine(10));
-        engines.add(new Engine(15));
-        engines.add(new Engine(36));
-        engines.add(new Engine(100));
-
-        ArrayList<Car> cars = new ArrayList();
-        cars.add(new Car(engines.get(1)));
-        cars.add(new Car(engines.get(2)));
-        cars.add(new Car(engines.get(3)));
-        cars.add(new Car(engines.get(2)));
-        cars.add(new Car(engines.get(2)));
-        cars.add(new Car(engines.get(0)));
-
-        for (Car car: cars){
-            System.out.println(car.toString());
-        }
+        // Використовуємо пульт управління від квадрокоптера Mavic_1 до квадрокоптера Mavic_2. Як бачимо, функції зависання
+        // у повітрі (метод hangs()), збільшення, та зменшення швидкості (increaseSpeed() та reduceSpeed()) уже немає , так як
+        // пульт управління від Mavic_1 не підтримує ці функції.
+        Mavic_1 mav2 = new Mavic_2();
+        mav2.on();
+        mav2.increaseHeight();
+        mav2.increaseHeight();
+        System.out.println("Висота:" + mav2.getHeight());
+        mav2.off();
 
     }
-
-
 }
